51412fc96e749c0e2fd71be8051a5ee5986b7bc6
jeanpierrelam76 <jeanpierrelam31@gmail.com>
2024-02-20 20:23:03 +0100

    rendu tp

---
+++ src/Form/VehiculeType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Vehicule;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VehiculeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('modele')
+            ->add('description')
+            ->add('date_creation')
+            ->add('image')
+            ->add('en_vente')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Vehicule::class,
+        ]);
+    }
+}
